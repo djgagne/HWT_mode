4a8a3ecd4f99e6086969a35a6360f089ac766ce4
ahijevyc <ahijevyc@ucar.edu>
2019-09-23 16:58:53 -0600

    Output image filename is Step_ID + ".png".
    Step_ID consists of the track id and the step (an integer counting up
    from zero with each time step along the track).
    
    Remove unneeded ncvar variable.
    
    Include Track_ID in the fine print. Fine print added in debug mode.
    
    Added comments.
    
    TODO:
    Define the snapshot padding in km, not degrees, so the
    size and aspect ratio don't change.

--- WRF_snapshots.py
+++ WRF_snapshots.py
@@ -24,16 +24,17 @@ import matplotlib.colors as colors
 parser = argparse.ArgumentParser(description = "Plot WRF and SPC storm reports")
 parser.add_argument("-v", "--variable", type=str, default= 'crefuh', help='netCDF variable name')
 parser.add_argument("-o", "--outdir", type=str, default='.', help="name of output path")
-parser.add_argument("-p", "--padding", type=float, nargs=4, help="padding on west, east, south and north side", default=[1,1,0.8,0.8])
+parser.add_argument("-p", "--padding", type=float, nargs=4, help="padding on west, east, south and north side", default=[1,1,0.8,0.8]) # TODO: define in units of km not degrees
 parser.add_argument("-f", "--force_new", action='store_true', help="overwrite any old outfile, if it exists")
 parser.add_argument("--counties", action='store_true', help="draw county borders (can be slow)")
-parser.add_argument('-i', "--idir", type=str, default="/glade/p/mmm/parc/sobash/NSC/3KM_WRF_POST_12sec_ts", help="path to input files")
-parser.add_argument('-t', "--tdir", type=str, default="/glade/work/sobash/NSC_objects/track_data_ncarstorm_3km_csv_refl", help="path to hagelslag track files")
+parser.add_argument('-i', "--idir", type=str, default="/glade/p/mmm/parc/sobash/NSC/3KM_WRF_POST_12sec_ts", help="path to WRF output files")
+parser.add_argument('-t', "--tdir", type=str, default="/glade/work/sobash/NSC_objects/track_data_ncarstorm_3km_csv_refl", help="path to hagelslag track-step files")
 parser.add_argument("initial_time", type=lambda d: datetime.datetime.strptime(d, '%Y%m%d%H'), help="model initialization date and hour, yyyymmddhh")
 parser.add_argument("valid_time", type=lambda d: datetime.datetime.strptime(d, '%Y%m%d%H'), help="model valid date and hour, yyyymmddhh")
 parser.add_argument("-d", "--debug", action='store_true')
 
 
+# Assign arguments to simple-named variables
 args = parser.parse_args()
 diagnostic_name = args.variable
 odir = args.outdir
@@ -49,21 +50,27 @@ debug = args.debug
 if debug:
     print(args)
 
+# Derive lead time and make sure it is between 12 and 36 hours. 
 lead_time = valid_time - initial_time
 
 if lead_time < datetime.timedelta(hours=12) or lead_time > datetime.timedelta(hours=36):
     print("lead_time:",lead_time, "not between 12 and 36 hours")
     sys.exit(1)
 
+
+# Read hagelslag track_step csv file into pandas DataFrame.
 tracks = tdir + '/' + initial_time.strftime('track_step_NCARSTORM_d01_%Y%m%d-0000_12.csv')
 if debug:
     print("reading csv file",tracks)
-    pdb.set_trace()
 df = pd.read_csv(tracks, parse_dates=['Run_Date', 'Valid_Date'])
+# Throw out everything except requested valid times.
 df = df[df.Valid_Date == valid_time]
 
+
+# Get wrfout filename
 wrfout = idir + '/' + initial_time.strftime('%Y%m%d%H') + '/' + valid_time.strftime('diags_d01_%Y-%m-%d_%H_%M_%S.nc')
 
+# Get color map, levels, and netCDF variable name appropriate for requested variable (from fieldinfo module).
 info = fieldinfo.fieldinfo[diagnostic_name]
 if debug:
     print("found fieldinfo in fieldinfo.py. Using",info)
@@ -79,18 +86,16 @@ if diagnostic_name not in wrfnc.variables:
     print("choices:", wrfnc.variables.keys())
     sys.exit(1)
 
-ncvar = wrfnc.variables[diagnostic_name]
-diagnostic_var = ncvar[:]
-if hasattr(ncvar, 'long_name'):
-    label = ncvar.long_name
-elif hasattr(ncvar, 'description'):
-    label = ncvar.description
-ncvar_units_str = ncvar.units
-
-# Get a 2D var from wrfout file so you have all the projection info
+# Get a 2D var from wrfout file. It has projection info.
 if debug:
     print("getvar...")
 cvar = getvar(Dataset(wrfout, 'r'),diagnostic_name)
+
+if hasattr(cvar, 'long_name'):
+    label = cvar.long_name
+elif hasattr(cvar, 'description'):
+    label = cvar.description
+cvar_units_str = cvar.units
 
 wrflat, wrflon = latlon_coords(cvar)
 # get cartopy mapping object
@@ -116,6 +121,7 @@ if debug:
     print("plt.contourf()...")
 cfill   = ax.contourf(to_np(wrflon), to_np(wrflat), to_np(cvar), levels=levels, cmap=cmap, transform=cartopy.crs.PlateCarree() )
 
+# Special case of composite reflectivity/UH 
 if args.variable == 'crefuh':
     uh = getvar(Dataset(wrfout, 'r'),info['fname'][1])
     print("Overlay UH",uh.max())
@@ -127,7 +133,7 @@ if args.variable == 'crefuh':
         print("uh has zero contour for some reason. Hide it")
         for i in cs2.collections:
             i.remove()
-               
+
 # Read my own county shape file.
 if args.counties:
     if debug:
@@ -142,7 +148,7 @@ if args.counties:
 
 
 # Color bar
-cb = plt.colorbar(cfill, ax=ax, format='%.0f', label=label+" ("+ncvar_units_str+")", 
+cb = plt.colorbar(cfill, ax=ax, format='%.0f', label=label+" ("+cvar_units_str+")", 
         shrink=0.55, orientation='horizontal')
 if cb.get_ticks().size < 9:
     # ticks=levels (label every level) if there is room.
@@ -150,16 +156,19 @@ if cb.get_ticks().size < 9:
 cb.ax.tick_params(labelsize='xx-small')
 cb.outline.set_linewidth(0.5)
 
-string  = os.path.realpath(wrfout)
-string += "\ncreated "+str(datetime.datetime.now(tz=None)).split('.')[0]
 
-if debug:
-    plt.annotate(s=string, xy=(260,5), xycoords='figure pixels', fontsize=4)
+fineprint = plt.annotate(s="", xy=(260,5), xycoords='figure pixels', fontsize=4)
 
+for lon,lat,stepid,trackid in zip(df.Centroid_Lon, df.Centroid_Lat,df.Step_ID,df.Track_ID):
 
-for lon,lat in zip(df.Centroid_Lon, df.Centroid_Lat):
+    pngfile = odir + '/' + stepid + ".png"
 
-    pngfile = odir + '/' + initial_time.strftime("%Y%m%d%H.") + valid_time.strftime("%Y%m%d%H.") + args.variable + ".{latitude}N_{longitude}E".format(latitude=lat,longitude=lon) + ".png"
+    string  = os.path.realpath(wrfout)
+    string += "\ntrack "+trackid
+    string += "\ncreated "+str(datetime.datetime.now(tz=None)).split('.')[0]
+
+    if debug:
+        fineprint.set_text(string)
 
     # If png already exists skip this file
     if not force_new and os.path.isfile(pngfile):
@@ -172,4 +181,4 @@ for lon,lat in zip(df.Centroid_Lon, df.Centroid_Lat):
 if debug: pdb.set_trace()
 plt.close(fig)
 print("Run this command to create a montage")
-print("montage -crop 595x595+260+10 -geometry 70% -tile 5x4 2*png t.png")
+print("montage -crop 595x595+260+10 -geometry 70% -tile 5x4 d01*png t.png")
