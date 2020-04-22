bba1f3bace704068c4ffad222370bd2bc8adc75c
djgagne@ou.edu <djgagne@ou.edu>
2020-04-22 08:20:30 -0600

    Added cheyenne training script

--- casper_train_script.sh
+++ casper_train_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #SBATCH --job-name=train_m_cnn
-#SBATCH ---account=NAML0001
+#SBATCH --account=NAML0001
 #SBATCH --ntasks=16
 #SBATCH --cpus-per-task=1
 #SBATCH --time=2:00:00
@@ -12,4 +12,4 @@
 #SBATCH --mail-user=dgagne@ucar.edu
 module load cuda/10.1
 export PATH="/glade/u/home/dgagne/miniconda3/envs/goes/bin:$PATH"
-python -u train_mode_cnn.py config/ws_mode_cnn_train.yml -t -i
+python -u train_mode_cnn.py config/ws_mode_cnn_train.yml -t -i -p
