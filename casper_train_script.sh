31379112c97e9fd9e8c191bef9507cb8709758c0
djgagne@ou.edu <djgagne@ou.edu>
2020-05-04 16:15:44 -0600

    Updated config files and interp

--- casper_train_script.sh
+++ casper_train_script.sh
@@ -13,5 +13,5 @@
 #SBATCH --reservation=casper_8xV100
 module load cuda/10.1
 export PATH="/glade/u/home/dgagne/miniconda3/envs/goes/bin:$PATH"
-python -u train_mode_cnn.py config/ws_mode_cnn_train_20200430.yml -t -i -p
+#python -u train_mode_cnn.py config/ws_mode_cnn_train_20200430.yml -t -i -p
 python -u train_mode_cnn.py config/ws_mode_cnn_train_20200430_grpl.yml -t -i -p
