f0c883e08181f1536ed7209d83ab3978efa44aeb
djgagne@ou.edu <djgagne@ou.edu>
2020-05-04 17:24:30 -0600

    Fixed bugs in interpretation plotting

--- hwtmode/interpretation.py
+++ hwtmode/interpretation.py
@@ -3,7 +3,7 @@ from sklearn.metrics import roc_auc_score
 import matplotlib.pyplot as plt
 from os.path import join
 from scipy.ndimage import gaussian_filter
-
+import pandas as pd
 
 def corr_coef_metric(y_true, y_pred):
     return np.corrcoef(y_true, y_pred)[0, 1]
@@ -98,9 +98,11 @@ def plot_saliency_composites(out_path, model_name, saliency_data, neuron_activat
 def plot_top_activations(out_path, model_name, x_data, meta_df, neuron_activations, neuron_scores, saliency_data,
                          variable_name, panel_size=16, figsize_scale=3.0, out_format="png", dpi=200, plot_kwargs=None,
                          colorbar_loc=(0.93, 0.1, 0.02, 0.8)):
+    if plot_kwargs is None:
+        plot_kwargs = {}
     fig_rows = int(np.floor(np.sqrt(panel_size)))
     fig_cols = int(np.ceil(panel_size / fig_rows))
-    for neuron_number in range(neuron_activations):
+    for neuron_number in range(neuron_scores.size):
         n_rank = neuron_activations[f"neuron_{neuron_number:03d}"].argsort()[::-1].values
         fig, axes = plt.subplots(fig_rows, fig_cols,
                                  figsize=(fig_cols * figsize_scale, fig_rows * figsize_scale),
@@ -114,13 +116,15 @@ def plot_top_activations(out_path, model_name, x_data, meta_df, neuron_activatio
             ax.contour(-sal_ex[a], 6, vmin=-sal_max, vmax=sal_max, cmap="RdBu_r")
             ax.set_xticks(np.arange(0, 32, 8))
             ax.set_yticks(np.arange(0, 32, 8))
-            ax.text(0, 0, meta_df.loc[n_rank[a], "time"])
+            ex_n_score = neuron_activations.loc[n_rank[a], f"neuron_{neuron_number:03d}"]
+            ax.text(0, 0, pd.Timestamp(meta_df.loc[n_rank[a], "time"]).strftime("%Y-%m-%d %HZ") + " S:{0:0.2f}".format(ex_n_score),
+                    bbox=dict(facecolor='white', alpha=0.5))
         if pc is not None:
             cb_ax = fig.add_axes(colorbar_loc)
             cbar = fig.colorbar(pc, cax=cb_ax)
         fig.suptitle(f"Neuron {neuron_number} Top Activated Storms, Score: {neuron_scores[neuron_number]:0.3f}",
                      fontsize=14, y=0.95)
-        plt.savefig(join(out_path, f"top_activations_neuron_{neuron_number:03d}_{model_name}.{out_format}"),
+        plt.savefig(join(out_path, f"top_activations_neuron_{variable_name}_{neuron_number:03d}_{model_name}.{out_format}"),
                     dpi=dpi, bbox_inches="tight")
         plt.close()
     return
