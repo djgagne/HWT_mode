bab4fe0b9b751bdd0196ec198ff221676514ecb9
Charlie Becker <charles11becker@gmail.com>
2021-02-27 12:01:19 -0700

    Config / submit script changes;

--- casper_run_script.sh
+++ casper_run_script.sh
@@ -3,15 +3,15 @@
 #SBATCH --account=NAML0001
 #SBATCH --ntasks=4
 #SBATCH --cpus-per-task=1
-#SBATCH --time=1:00:00
+#SBATCH --time=00:30:00
 #SBATCH --partition=dav
-#SBATCH --mem=128G
-#SBATCH --output=run_mode_cnn.%j.out
+#SBATCH --mem=64G
+#SBATCH --output=run_mode_cnn.out
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=dgagne@ucar.edu
-#SBATCH --reservation=casper_8xV100
+#SBATCH --mail-user=cbecker@ucar.edu
 #SBATCH --gres=gpu:v100:1
 module load cuda/10.1
-export PATH="/glade/u/home/dgagne/miniconda3/envs/goes/bin:$PATH"
-export PROJ_LIB="/glade/u/home/dgagne/miniconda3/envs/goes/share/proj"
-python -u run_mode_cnn.py config/ws_mode_cnn_run_20200504_uh.yml 
+conda activate hwt
+#export PATH="/glade/u/home/dgagne/miniconda3/envs/goes/bin:$PATH"
+#export PROJ_LIB="/glade/u/home/dgagne/miniconda3/envs/goes/share/proj"
+python -u run_mode_cnn.py config/charlie_run.yml
--- run_mode_cnn.py
+++ run_mode_cnn.py
@@ -4,9 +4,10 @@ from os.path import exists, join
 from os import makedirs
 import pandas as pd
 import numpy as np
+import joblib
 from hwtmode.data import load_patch_files, combine_patch_data, min_max_scale, storm_max_value, get_meta_scalars
 from hwtmode.models import load_conv_net
-from hwtmode.analysis import plot_storm_mode_analysis_map
+#from hwtmode.analysis import plot_storm_mode_analysis_map
 
 def main():
     parser = argparse.ArgumentParser()
@@ -28,6 +29,7 @@ def main():
     print(list(meta.variables.keys()))
     meta_df = get_meta_scalars(meta)
     models = {}
+    gmms = {}
     neuron_activations = {}
     if not exists(config["activation_path"]):
         makedirs(config["activation_path"])
@@ -48,28 +50,38 @@ def main():
         print("Neuron activation shape:", neuron_activations[model_name].shape)
         run_dates = pd.DatetimeIndex(neuron_activations[model_name]["run_date"].unique())
         print(run_dates)
-        for run_date in run_dates:
-            print(run_date)
-            rdi = neuron_activations[model_name]["run_date"] == run_date
-            print("Run storm count", np.count_nonzero(rdi))
-            run_date_str = run_date.strftime(config["date_format"])
-            na_file = join(config["activation_path"],
-                           f"neuron_activations_{model_name}_{run_date_str}.csv")
-            neuron_activations[model_name].loc[rdi].to_csv(na_file, index_label="index")
-            meta_run = meta.isel(p=np.where(rdi)[0])
-            print(run_date.strftime("%Y-%m-%d") + " plot all storms")
-            plot_storm_mode_analysis_map(neuron_activations[model_name].loc[rdi],
-                                         meta_run, config["models"][model_name],
-                                         run_date, 1, 35, model_name, config["activation_path"], period_total=True)
-            plot_storm_mode_analysis_map(neuron_activations[model_name].loc[rdi],
-                                         meta_run, config["models"][model_name],
-                                         run_date, 12, 35, model_name, config["activation_path"], period_total=True)
-            print(run_date.strftime("%Y-%m-%d") + " plot hourly storms")
-            plot_storm_mode_analysis_map(neuron_activations[model_name].loc[rdi],
-                                         meta_run, config["models"][model_name],
-                                         run_date, config["start_hour"], config["end_hour"], model_name,
-                                         config["activation_path"],
-                                         period_total=False)
+
+        gmms[model_name] = joblib.load(join(config["out_path"], f'{model_name}.gmm'))
+        gmms[model_name].predict(neuron_activations[model_name].loc[:, neuron_columns])
+        gmms[model_name].predict_proba(neuron_activations[model_name].loc[:, neuron_columns]).max(axis=1)
+        labels = joblib.load(join(config["out_path"], f'{model_name}_gmm_labels.dict'))
+
+        # for run_date in run_dates:
+        #     print(run_date)
+        #     rdi = neuron_activations[model_name]["run_date"] == run_date
+        #     print("Run storm count", np.count_nonzero(rdi))
+        #     run_date_str = run_date.strftime(config["date_format"])
+        #     na_file = join(config["activation_path"],
+        #                    f"neuron_activations_{model_name}_{run_date_str}.csv")
+        #     neuron_activations[model_name].loc[rdi].to_csv(na_file, index_label="index")
+        #     meta_run = meta.isel(p=np.where(rdi)[0])
+        #     print(run_date.strftime("%Y-%m-%d") + " plot all storms")
+            # plot_storm_mode_analysis_map(neuron_activations[model_name].loc[rdi],
+            #                              meta_run, config["models"][model_name],
+            #                              run_date, 1, 35, model_name, config["activation_path"], period_total=True)
+            # plot_storm_mode_analysis_map(neuron_activations[model_name].loc[rdi],
+            #                              meta_run, config["models"][model_name],
+            #                              run_date, 12, 35, model_name, config["activation_path"], period_total=True)
+            # print(run_date.strftime("%Y-%m-%d") + " plot hourly storms")
+            # plot_storm_mode_analysis_map(neuron_activations[model_name].loc[rdi],
+            #                              meta_run, config["models"][model_name],
+            #                              run_date, config["start_hour"], config["end_hour"], model_name,
+            #                              config["activation_path"],
+            #                              period_total=False)
+
+            ### LOAD GMM, make predictions, append predictions to CSV ###
+
+
     return
 
 
