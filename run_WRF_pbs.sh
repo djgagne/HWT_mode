8d212df7923d35c0ac9a11a608b45a23d8876899
Charlie Becker <charles11becker@gmail.com>
2021-12-13 12:55:01 -0700

    Minor change to submit script

--- run_WRF_pbs.sh
+++ run_WRF_pbs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
 #PBS -N mode_cnn
 #PBS -A NAML0001
-#PBS -l walltime=00:30:00
+#PBS -l walltime=01:00:00
 #PBS -o hwt_WRF.out
-#PBS -e hwt_WRF.err
+#PBS -e hwt_WRF.out
 #PBS -q casper
-#PBS -l select=1:ncpus=18:mem=64GB
+#PBS -l select=1:ncpus=12:mem=64GB
 #PBS -m a
 #PBS -M cbecker@ucar.edu
-conda activate hagelslag
-python -u /glade/scratch/cbecker/hagelslag/bin/hsdata /glade/scratch/cbecker/hagelslag/config/rt_WRF_2021.config -p 18
+#conda activate hagelslag
+#python -u /glade/scratch/cbecker/hagelslag/bin/hsdata /glade/scratch/cbecker/hagelslag/config/rt_WRF_2021.config -p 18
 conda activate hwt
-python -u /glade/work/cbecker/HWT_mode/run_mode_cnn.py /glade/work/cbecker/HWT_mode/config/WRF_run_cnn_gmm.yml
+python -u /glade/work/cbecker/HWT_mode/run_mode_cnn.py /glade/work/cbecker/HWT_mode/config/WRF_run_cnn_gmm.yml -e
