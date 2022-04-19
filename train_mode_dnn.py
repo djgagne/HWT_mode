3aeb1ebb547edb4dcc465b313bfab15b2564af7c
ahijevyc <ahijevyc@ucar.edu>
2022-04-19 12:04:23 -0600

    random seed
    include brier_skill_score instead of importing (imported version
    does not like Tensors)
    
    use logging instead of print statements

--- train_mode_dnn.py
+++ train_mode_dnn.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 def baseline_model(input_dim=None, name=None,numclasses=None, neurons=16, layer=2, optimizer='adam', dropout=0):
 
     # Discard any pre-existing version of the model.
@@ -114,17 +112,26 @@ def label_longname_dict():
     d = {"Q1":"QLCS\nQ1+Q2+S2", "S1":"Supercell\nS1+S3", "D1":"Disorganized\nD1+D2"}
     return d
 
+# Tried from hwtmode.evaluation import brier_skill_score, but AttributeError: 'Tensor' object has no attribute 'mean'
+def brier_skill_score(obs, preds):
+    bs = K.mean((preds - obs) ** 2)
+    obs_climo = K.mean(obs, axis=0) # use each observed class frequency instead of 1/nclasses. Only matters if obs is multiclass.
+    bs_climo = K.mean((obs - obs_climo) ** 2)
+    bss = 1.0 - bs/(bs_climo+K.epsilon()) 
+    return bss
+
 import argparse
 import glob
 import joblib
+import logging
 import matplotlib.pyplot as plt
-from hwtmode.evaluation import brier_skill_score
 import numpy as np
 import os
 import pandas as pd
 import pdb
 import pickle
 from hwtmode.data import decompose_circular_feature
+import random
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import cross_val_score, GridSearchCV, KFold 
@@ -153,6 +160,7 @@ def main():
     parser.add_argument('--labelpick', type=str, choices=["first", "last", "all"], default="all", help="how to handle multiple labels on same storm")
     parser.add_argument('--model_fname', type=str, help="filename of machine learning model")
     parser.add_argument('--neurons', type=int, nargs="+", default=[12], help="number of neurons in each nn layer")
+    parser.add_argument('--seed', type=int, default=None, help="random number seed for reproducability")
     parser.add_argument('--segmentations', nargs="*", type=str, default=["hyst"], help="storm segmentation approaches")
     parser.add_argument('--splittime', type=lambda s: pd.to_datetime(s), default=pd.to_datetime("2013-06-25"), help="train with storms before this time; test this time and after")
     parser.add_argument('--suite', type=str, default='basic', choices=feature_dict.keys(), help="name for group of features")
@@ -171,30 +179,31 @@ def main():
     labelpick             = args.labelpick
     savedmodel            = args.model_fname
     neurons               = args.neurons
+    seed                  = args.seed
     segmentations         = args.segmentations
     train_test_split_time = args.splittime
     suite                 = args.suite
 
-    print(args)
+    logging.debug(args)
 
-    ### NEURAL NETWORK PARAMETERS ###
+    if seed:
+        logging.info(f"random seed {seed}")
+        random.seed(seed)
+        np.random.seed(seed)
+        tf.random.set_seed(seed)
 
     dataset = 'NSC'
     trained_models_dir = '/glade/work/ahijevyc/NSC_objects'
-    if savedmodel:
-        pass
-    else:
+    if not savedmodel:
         savedmodel = f"{suite}." + ".".join(label_longname_dict().keys()) + f".{labelpick}label.confmin{confmin}.{neurons[0]}n.ep{epochs[0]}.bs{batch_sizes[0]}"
 
-    ##################################
-
     mcd = os.getenv("TMPDIR", "/glade/ahijevyc/scratch/temp") + f"/HWT_mode_output/atts_and_expertlabels_"+".".join(segmentations)+".csv"
-    print(f'Reading {mcd}')
+    logging.info(f'Reading {mcd}')
     if os.path.exists(mcd):
         df = pd.read_csv(mcd, parse_dates=["Run_Date","Valid_Date","labeltime"])
         df = df.sort_values("labeltime") # so you can pick first (or last) label of each storm
     else:
-        print(f"Use ~ahijevyc/bin/hagelslag_obj_pdf.py to make {mcd}")
+        logging.error(f"Use ~ahijevyc/bin/hagelslag_obj_pdf.py to make {mcd}")
         sys.exit(1)
 
     df.info() # tried adding show_counts=True but got TypeError: info() got an unexpected keyword argument 'show_counts'
@@ -203,12 +212,12 @@ def main():
     # Confidence filter training set (not testing)
     lowconf = (df.conf < confmin) & (df.Valid_Date < train_test_split_time) # just Training
     #lowconf = df.conf < confmin # Training and testing (not recommended by DJG) 
-    print(f"dropping {lowconf.sum()}/{len(df)} expert labels with confidence < {confmin} from training set")
+    logging.info(f"dropping {lowconf.sum()}/{len(df)} expert labels with confidence < {confmin} from training set")
     df = df[~lowconf]
 
     # Labeltime filter
     nlate = (df.labeltime >= freezetime).sum()
-    print(f"dropping {nlate}/{len(df)} labeltimes {freezetime} or later")
+    logging.info(f"dropping {nlate}/{len(df)} labeltimes {freezetime} or later")
     df = df[df.labeltime < freezetime]
 
     # Multiple labels filter
@@ -225,8 +234,8 @@ def main():
     test_indices = ~train_indices
     df["split"] = "train"
     df.loc[test_indices,"split"] = "test"
-    print(f"{train_indices.sum()} ({100.*train_indices.sum()/len(df):.0f}%) training cases Valid_Date < {train_test_split_time}")
-    print(f"{test_indices.sum()} ({100.*test_indices.sum()/len(df):.0f}%) test cases Valid_Date >= {train_test_split_time}")
+    logging.info(f"{train_indices.sum()} ({100.*train_indices.sum()/len(df):.0f}%) training cases Valid_Date < {train_test_split_time}")
+    logging.info(f"{test_indices.sum()} ({100.*test_indices.sum()/len(df):.0f}%) test cases Valid_Date >= {train_test_split_time}")
 
     labels = df['label'].astype("category")
     labels = labels.cat.reorder_categories(["Q1", "Q2", "S1", "S2", "S3", "D1", "D2"], ordered=True)
@@ -241,7 +250,7 @@ def main():
 
     value_cnts = True
     if value_cnts:
-        print("plotting class histogram")
+        logging.info("plotting class histogram")
         df["label"] = labels
         bax = df[["label","split"]].value_counts().unstack().plot.bar(grid=True)
         for container in bax.containers:
@@ -249,7 +258,7 @@ def main():
         plt.tight_layout()
         ofile = ".".join(label_longname_dict().keys())+f".{labelpick}labels.value_counts.png"
         plt.savefig(ofile)
-        print(os.path.realpath(ofile))
+        logging.info(os.path.realpath(ofile))
         plt.clf()
 
     oneclass = False
@@ -261,7 +270,7 @@ def main():
 
     labels = labels.cat.remove_unused_categories()
     numclasses = labels.nunique()
-    print(f"{numclasses} classes")
+    logging.info(f"{numclasses} classes")
     # assert all(encoder.classes_ == sorted(labels.cat.categories)) # normalize_and_topn.ipynb assumes labels were encoded alphabetically. TODO: is this still true?
     # Tried LabelEncoder, but it encodes alphabetically and does not honor Pandas category order.
     onehotlabels = label_binarize(labels, classes=labels.cat.categories) # label_binarize over LabelEncoder/LabelBinarizer because it allows you to specify order of classes.
@@ -277,14 +286,14 @@ def main():
 
     df = df[features]
     
-    print("normalize data")
+    logging.info("normalize data")
     #scaler = pickle.load(open('/glade/work/ahijevyc/NSC_objects/scaler.pkl', 'rb'))
     #scaler = scaler[features] 
     # We'll want to normalize the whole thing.
     #norm_in_data = (df - scaler.loc["mean"] ) / scaler.loc["std"]
     scaler = StandardScaler().fit(df[train_indices])
     norm_in_data = scaler.transform(df)
-    print('done normalizing')
+    logging.info('done normalizing')
 
     nn = True
     rf = False
@@ -303,12 +312,12 @@ def main():
             grid_result = grid.fit(norm_in_data[train_indices], onehotlabels[train_indices], 
                     validation_data=(norm_in_data[test_indices], onehotlabels[test_indices]), verbose=2)
             # summarize results
-            print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+            logging.info("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
             means = grid_result.cv_results_['mean_test_score']
             stds = grid_result.cv_results_['std_test_score']
             params = grid_result.cv_results_['params']
             for mean, stdev, param in zip(means, stds, params):
-                print("%f (%f) with: %r" % (mean, stdev, param))
+                logging.info("%f (%f) with: %r" % (mean, stdev, param))
             if clobber or not os.path.exists(savedmodel):
                 grid_result.best_estimator_.model.save(savedmodel)
         else:
@@ -316,14 +325,16 @@ def main():
             for i in range(nfit):
                 model_i = f"nn/nn_{savedmodel}_{i}"
                 if not clobber and os.path.exists(model_i):
-                    print(f"loading {model_i}")
+                    logging.info(f"loading {model_i}")
                     custom_objects = {"brier_score":brier_score, "brier_skill_score":brier_skill_score}
                     models.append(load_model(model_i, custom_objects=custom_objects))
                     history = models[-1].history
                 else:
                     model = baseline_model(input_dim=norm_in_data.shape[1],numclasses=numclasses,neurons=neurons[0], name=f"fit_{i}")
-                    history = model.fit(norm_in_data[train_indices], onehotlabels[train_indices], class_weight=class_weight[0], sample_weight=sample_weight[0], 
-                        epochs=epochs[0], validation_data=(norm_in_data[test_indices], onehotlabels[test_indices]), batch_size=batch_sizes[0], verbose=2)
+                    history = model.fit(norm_in_data[train_indices], onehotlabels[train_indices], class_weight=class_weight[0], 
+                            sample_weight=sample_weight[0], epochs=epochs[0], 
+                            validation_data=(norm_in_data[test_indices], onehotlabels[test_indices]), 
+                            batch_size=batch_sizes[0], verbose=2)
                     models.append(model)
                     model.save(model_i)
                     model.save(model_i+".h5", save_format='h5', overwrite=clobber)
@@ -343,7 +354,7 @@ def main():
             y_preds = []
             tbles = pd.DataFrame()
             for model in models:
-                print(model.name)
+                logging.info(model.name)
                 y_pred = model.predict(norm_in_data[test_indices])
                 tble = vx_tble(labels, onehotlabels[test_indices], y_pred)
                 tbles = tbles.append(tble)
@@ -363,7 +374,7 @@ def main():
             print(vx_tble(labels, onehotlabels[test_indices], y_pred))
             statcurves = True
             if statcurves:
-                print("reliability diagram, histogram, & ROC curve")
+                logging.info("reliability diagram, histogram, & ROC curve")
                 fig = plt.figure(figsize=(10,7))
                 ax1 = plt.subplot2grid((3,2), (0,0), rowspan=2)
                 ax2 = plt.subplot2grid((3,2), (2,0), rowspan=1, sharex=ax1)
@@ -376,12 +387,12 @@ def main():
                 fig.text(0.5, 0.01, ' '.join(features), wrap=True, fontsize=6)
                 ofile = f"nn/{savedmodel}.statcurves.png"
                 fig.savefig(ofile)
-                print(os.path.realpath(ofile))
+                logging.info(os.path.realpath(ofile))
                 plt.clf()
             confusion_matrix = True
             if confusion_matrix:
                 from scikitplot.metrics import plot_confusion_matrix
-                print("confusion matrix")
+                logging.info("confusion matrix")
                 fig, ax = plt.subplots()
                 y_pred_class = labels.cat.categories[np.argmax(y_pred, axis=1)]
                 cm = plot_confusion_matrix(labels[test_indices], y_pred_class, labels.cat.categories, normalize=True, ax=ax)
@@ -389,12 +400,12 @@ def main():
                 fig.tight_layout()
                 ofile = f"nn/{savedmodel}.confusion_matrix.png"
                 plt.savefig(ofile)
-                print(os.path.realpath(ofile))
+                logging.info(os.path.realpath(ofile))
                 plt.clf()
         df = get_other_validation_data()
         df = df[features]
         norm_in_data2021hwt = scaler.transform(df)
-        print('done normalizing another test set')
+        logging.info('done normalizing another test set')
         preds = []
         print('predicting with multiple models',end='')
         for model in models:
@@ -404,13 +415,13 @@ def main():
         mean_pred = np.array(preds).mean(axis=0)
         ofile = f"nn/{savedmodel}.csv"
         df.join(pd.DataFrame(mean_pred, index=df.index, columns=labels.cat.categories)).to_csv(ofile)
-        print("created",os.path.realpath(ofile))
+        logging.info(f"created {os.path.realpath(ofile)}")
 
     if rf:
         rf_savedmodel = "rf/rf_"+savedmodel
-        print(rf_savedmodel)
+        logging.info(rf_savedmodel)
         if not clobber and os.path.exists(rf_savedmodel):
-            print("loading saved rf")
+            logging.info("loading saved rf")
             rf = joblib.load(rf_savedmodel)
         else:
             rf = RandomForestClassifier(n_estimators=200, max_depth=50, min_samples_split=2, oob_score=True, criterion="gini", random_state=None, n_jobs=-1)
@@ -424,7 +435,7 @@ def main():
         print(importances.head(80))
         df = get_other_validation_data()
         norm_in_data = scaler.transform(df)
-        print('done normalizing')
+        logging.info('done normalizing')
         preds = rf.predict(norm_in_data)
         df.join(pd.DataFrame(preds, index=df.index, columns=labels.cat.categories)).to_csv(f"{rf_savedmodel}.csv")
 if __name__ == "__main__":
