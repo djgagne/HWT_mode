40841eb58bca255b1b00207616652b5c1b65d440
ahijevyc <ahijevyc@ucar.edu>
2022-05-11 15:08:51 -0600

    used djg's fixes instead

--- train_mode_dnn.py
+++ train_mode_dnn.py
@@ -1,3 +1,30 @@
+import argparse
+import glob
+import joblib
+import logging
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import pdb
+import pickle
+from hwtmode.data import decompose_circular_feature
+import random
+import scalar2vector
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import cross_val_score, GridSearchCV, KFold 
+from sklearn.preprocessing import StandardScaler, label_binarize
+import statisticplot # ahijevyc's module
+import tensorflow as tf
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.metrics import MeanSquaredError, AUC
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+import sys, time
+import yaml
+
 def baseline_model(input_dim=None, name=None,numclasses=None, neurons=16, layer=2, optimizer='adam', dropout=0):
 
     # Discard any pre-existing version of the model.
@@ -120,33 +147,6 @@ def brier_skill_score(obs, preds):
     bss = 1.0 - bs/(bs_climo+K.epsilon()) 
     return bss
 
-import argparse
-import glob
-import joblib
-import logging
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import pandas as pd
-import pdb
-import pickle
-from hwtmode.data import decompose_circular_feature
-import random
-import scalar2vector
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import cross_val_score, GridSearchCV, KFold 
-from sklearn.preprocessing import StandardScaler, label_binarize
-import statisticplot # ahijevyc's module
-import tensorflow.random
-from tensorflow.keras import backend as K
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.metrics import MeanSquaredError, AUC
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
-import sys, time
-import yaml
-
 def main():
     # =============Arguments===================
     parser = argparse.ArgumentParser(description = "train/predict neural network",
@@ -192,7 +192,7 @@ def main():
         logging.info(f"random seed {seed}")
         random.seed(seed)
         np.random.seed(seed)
-        tensorflow.random.set_seed(seed)
+        tf.random.set_seed(seed)
 
     dataset = 'NSC'
     trained_models_dir = '/glade/work/ahijevyc/NSC_objects'
