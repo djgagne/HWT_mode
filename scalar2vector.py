741864de01b0e59c19e230ec0c14cf232843007d
ahijevyc <ahijevyc@ucar.edu>
2022-05-11 15:15:56 -0600

    func is already in hwtmode/data.py

--- scalar2vector.py
+++ scalar2vector.py
@@ -3,19 +3,6 @@ import numpy as np
 import pandas as pd
 import sys
 
-# Decompose a circular feature like azimuth, LST, or orientation into its 1) sine and 2) cosine components.
-# Drop original circular feature, unless optional drop argument is set explicitly to False.
-def decompose_circ_feature(df: pd.DataFrame, *features, period=2*np.pi, drop=True):
-    for feature in features:
-        logging.info(f"{feature} sin and cos components, period={period}")
-        df[feature+"_sin"] = np.sin(df[feature] * 2*np.pi/period)
-        df[feature+"_cos"] = np.cos(df[feature] * 2*np.pi/period)
-        if drop:
-            logging.debug(f"drop {feature} column from dataframe")
-            df = df.drop(columns=feature)
-    return df
-
-
 def scalar2vector(*arg):
     logging.error("scalar2vector.scalar2vector() is depreciated. Use scalar2vector.uvmagnitude() instead.")
     sys.exit(1)
