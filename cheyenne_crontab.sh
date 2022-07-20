052ae7ae0d1ca37b0a2878505ab49b8c33626c33
Charlie Becker <charles11becker@gmail.com>
2022-07-20 16:15:41 -0600

    Add crontab script for real time runs

---
+++ cheyenne_crontab.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+12 * * * * /glade/work/cbecker/HWT_mode/rt_submit_wrapper.sh & >> /glade/scratch/cbecker/HRRR_HWT_2022_real_time/cron.log
+20 * * * * /glade/work/cbecker/HWT_mode/sync_to_AWSS3.sh
